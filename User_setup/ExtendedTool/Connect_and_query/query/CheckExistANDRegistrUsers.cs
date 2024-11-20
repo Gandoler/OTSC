a1bcbad89f7d0fc9368394a8655de3eedc886819
glkru <glkru@DESKTOP-M6A0VUC>
2024-11-20 18:07:10 +0300

    Начал первые запросы

---
+++ User_setup/ExtendedTool/Connect_and_query/query/CheckExistANDRegistrUsers.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Interface.ExtendedTool.Connect_and_query.query
+{
+    internal static class CheckExistANDRegistrUsers
+    {
+        public static bool CheckOFExistUser(MySqlConnection connection,string login,string psw)
+        {
+            string query = 
+        }
+
+    }
+}
