1b61c53da39e77558e1f0d59bec7a71bf15e2e35
glkru <glkru@DESKTOP-M6A0VUC>
2024-11-22 12:42:18 +0300

    стартанул писать запросы для логирования

--- User_setup/ExtendedTool/Connect_and_query/query/CheckExistANDRegistrUsers.cs
+++ User_setup/ExtendedTool/Connect_and_query/query/CheckExistANDRegistrUsers.cs
@@ -11,7 +11,59 @@ namespace User_Interface.ExtendedTool.Connect_and_query.query
     {
         public static bool CheckOFExistUser(MySqlConnection connection,string login,string psw)
         {
-            string query = 
+            string query = "SELECT COUNT(*) FROM Users WHERE Username = @username and Password = @psw;";
+            try
+            {
+                using (var query_command = new MySqlCommand(query, connection))
+                {
+                    query_command.Parameters.AddWithValue("@username", login);
+                    query_command.Parameters.AddWithValue("@psw", psw);
+                    var result = query_command.ExecuteNonQuery();
+                    return Convert.ToInt32(result) == 1;
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+         
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            throw new Exception("ERORR WITH CHECK YOU IN DB");//хз как грамотно эту ошибку дернуть
+
+        }
+
+        public static bool RegistrUser(MySqlConnection connection, string login, string psw)
+        {
+
+            if (CheckOFExistUser(connection, login, psw))
+            {
+                MessageBox.Show("User exist already", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            string query = "INSERT INTO Users (Username, Password) VALUES (@username, @psw);";
+            try
+            {
+                using (var query_command = new MySqlCommand(query, connection))
+                {
+                    query_command.Parameters.AddWithValue("@username", login);
+                    query_command.Parameters.AddWithValue("@psw", psw);
+                    int rowsAdd = query_command.ExecuteNonQuery();
+                    return rowsAdd > 0;
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            throw new Exception("ERORR WITH ADD YOU IN DB");//хз как грамотно эту ошибку дернуть
         }
 
     }
