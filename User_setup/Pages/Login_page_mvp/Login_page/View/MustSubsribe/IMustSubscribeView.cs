beb2e31743a28105f95cddd3f155da5e75cd8356
glkru <glkru@DESKTOP-M6A0VUC>
2024-12-12 17:15:19 +0300

    Начла делать связь и первые фуннкции в вь.ю

---
+++ User_setup/Pages/Login_page_mvp/Login_page/View/MustSubsribe/IMustSubscribeView.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTSC_ui.Pages.Login_page_mvp.Login_page.View.MustSubsribe
+{
+    internal interface IMustSubscribeView
+    {
+        //exit button
+        event Action ExitButtonClick;
+        event Action ExitButtonEntered;
+        event Action ExitButtonLeavd;
+
+        //tg button
+        event Action TgBotButtonClickClick;
+
+
+        //code Field
+        event EventHandler<EventArgs> CodeFieldTextChenged;
+        event Action LeaveCodeField;
+
+
+        //enter code button
+        event Action CodeEnterButton;
+
+
+
+
+        void MakeErrorImageEnable();
+        void MakeErrorImageDisable();
+        void MakeEnterCodeButtonEnable();
+        void MakeEnterCodeEnterButtonDisable();
+        void MakeExitButtonForeRed();
+        void MakeExitButtonForeBlack();
+
+    }
+}
---
+++ User_setup/Pages/Login_page_mvp/Login_page/View/MustSubsribe/MustSubscribeForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OTSC_ui.Pages.Login_page_mvp.Login_page.View.MustSubsribe;
+
+namespace OTSC_ui.Pages.Login_page_mvp.Login_page.MustSubsribe
+{
+    public partial class MustSubscribeForm : Form, IMustSubscribeView
+    {
+        public MustSubscribeForm()
+        {
+            InitializeComponent();
+
+
+            //exit button
+            ExitButton.Click += (s, e) => ExitButtonClick?.Invoke();
+            ExitButton.Enter += (s, e) => ExitButtonEntered?.Invoke();
+            ExitButton.Leave += (s, e) => ExitButtonLeavd?.Invoke();
+
+
+            //tg button
+            TgButton.Click += (s, e) => TgBotButtonClickClick?.Invoke();
+
+
+            //code Field
+            CodeFieldTextBox.TextChanged += (s, e) => CodeFieldTextChenged?.Invoke(this, CodeFieldTextBox.);
+
+            //enter code button
+    }
+
+        public event EventHandler<EventArgs>? CodeFieldTextChenged;
+        public event Action? TgBotButtonClickClick;
+        public event Action? LeaveCodeField;
+        public event Action? CodeEnterButton;
+        public event Action? ExitButtonClick;
+        public event Action? ExitButtonEntered;
+        public event Action? ExitButtonLeavd;
+
+        public void MakeEnterCodeButtonEnable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MakeEnterCodeEnterButtonDisable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MakeErrorImageDisable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MakeErrorImageEnable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MakeExitButtonForeBlack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MakeExitButtonForeRed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
